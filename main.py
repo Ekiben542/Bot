4afdb895c9b45b30e11835ef8c0fa4cf784f8360
Ekiben542 <71870048+Ekiben542@users.noreply.github.com>
2023-07-29 19:20:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 app = Quart(__name__)
 
-async def give_reward_role_to_last_sent_msg_of_user(channel, role_name):
-    last_message = channel.last_message
-    if last_message.content.startswith('k!quiz n') and last_message.content[8] in '12345' and last_message.content[10] in '123456789':
-        # split the message content by spaces to get the individual arguments
-        args = last_message.content.split()
-        # check if the third argument (the number of questions) is "1"
-        if len(args) > 2 and args[2] == "1":
-            return
-    role = discord.utils.get(channel.guild.roles, name=role_name)
-    async for msg in channel.history(limit=10):
-        if not msg.embeds:
-            await msg.author.add_roles(role)
-            return
+async def give_reward_role_to_last_sent_msg_of_user(channel, role_name, or_var):
+    if or_var:
+        last_message = channel.last_message
+        if last_message.content.startswith('k!quiz n') and last_message.content[8] in '12345' and last_message.content[10] in '123456789':
+            # split the message content by spaces to get the individual arguments
+            args = last_message.content.split()
+            # check if the third argument (the number of questions) is "1"
+            if len(args) > 2 and args[2] == "1":
+                return
+        role = discord.utils.get(channel.guild.roles, name=role_name)
+        async for msg in channel.history(limit=10):
+            if not msg.embeds:
+                await msg.author.add_roles(role)
+                return
 
 
 @bot.event
@@ -28,17 +29,14 @@ async def on_message(message):
         return
 
     if message.channel.id == 1117024608651063377 or message.channel.id == 1118401512478097499 or message.channel.id == 1124293569260310621:
-        or_flag = True
+
         if message.embeds:
             embed = message.embeds[0]
+            or_var = True
             if embed.title and 'Quiz Starting in 5 seconds' in embed.title:
                 for field in embed.fields:
-                    if field.name == 'Score limit':
-                        score_limit = int(field.value)
-                        if score_limit <= 10:
-                            or_flag = False
-                        else:
-                            or_flag = True
+                    if field.name == 'Score limit' and field.value in '123456789':
+                        or_var = False
                         break
             for n in range(1, 6):
                 if embed.title and f'JLPT N{n} Reading Quiz Ended' in embed.title:
@@ -46,13 +44,13 @@ async def on_message(message):
                         if field.name.startswith('Unanswered Questions'):
                             unanswered_questions = eval(field.name.split(' ')[-1])
                             if unanswered_questions is None or unanswered_questions <= 3:
-                                await give_reward_role_to_last_sent_msg_of_user(message.channel, f'JLPT N{n}')
+                                await give_reward_role_to_last_sent_msg_of_user(message.channel, f'JLPT N{n}', or_var)
                             return
                         elif field.name == 'Final Scores':
-                            await give_reward_role_to_last_sent_msg_of_user(message.channel, f'JLPT N{n}')
+                            await give_reward_role_to_last_sent_msg_of_user(message.channel, f'JLPT N{n}', or_var)
                             return
-    if or_flag:
-        await bot.process_commands(message)
+      
+    await bot.process_commands(message)
 
 @bot.command(name='test')
 async def test(ctx):
